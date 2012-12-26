01f7464b6d2dd6e73855abf91c0a8049100687b6
David Weinstein <dweinst@insitusec.com>
2012-12-26 16:19:55 -0500

    fix formatting a little.

--- src/com/insitusec/androidreboot/AndroidReboot.java
+++ src/com/insitusec/androidreboot/AndroidReboot.java
@@ -19,13 +19,13 @@ import android.widget.Toast;
 public class AndroidReboot extends Activity {
 	private static final String DTAG = "AndroidReboot";
 	private static final String baseDirectory = "/sys/kernel/debug";
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_android_reboot);
 		final Button btnLaunch = (Button) findViewById(R.id.button1);
-		
+
 		/* specific files that have caused crashes. (credit in comment)
 		 * final Map<String, String> files;
 		 * files.put("gnex-cdma", "/sys/kernel/debug/usb/ehci/ehci-omap.0/registers"); // @insitusec
@@ -37,47 +37,47 @@ public class AndroidReboot extends Activity {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				
+
 				// first loop through known files
-				
+
 				Queue<String> fileQueue = new PriorityQueue<String>();
 				fileQueue.add(baseDirectory);
 				do { 
 					String curPath = fileQueue.remove();
-					
+
 					File file = new File( curPath );	
 					File[] fileList = file.listFiles();
 
-				        for ( File f : fileList ) {
-				            if ( f.isDirectory() ) {
-				            	fileQueue.add(f.getAbsolutePath());
-				            }
-				            
-				            else {
-				            	FileInputStream ohCrapFis = null;
-								try {
-									Log.d(DTAG, String.format("Testing file: %s (take note if we crash)", f));
-									byte[] buffer = new byte[1024];
-									ohCrapFis = new FileInputStream(f);
-									ohCrapFis.read(buffer);
-								} catch (FileNotFoundException e) {
-									
-									// TODO Auto-generated catch block
-									// Toast.makeText(getApplicationContext(), "You're ok!", Toast.LENGTH_LONG).show();
+					for ( File f : fileList ) {
+						if ( f.isDirectory() ) {
+							fileQueue.add(f.getAbsolutePath());
+						}
 
-								} catch (IOException e) {
-									// TODO Auto-generated catch block
-									//Log.d(DTAG, "File " + ohCrap.getAbsolutePath() + "had an IO Exception");
-									//e.printStackTrace();
-								}
-				            }
-				            
-				        } // for
+						else {
+							FileInputStream ohCrapFis = null;
+							try {
+								Log.d(DTAG, String.format("Testing file: %s (take note if we crash)", f));
+								byte[] buffer = new byte[1024];
+								ohCrapFis = new FileInputStream(f);
+								ohCrapFis.read(buffer);
+							} catch (FileNotFoundException e) {
+
+								// TODO Auto-generated catch block
+								// Toast.makeText(getApplicationContext(), "You're ok!", Toast.LENGTH_LONG).show();
+
+							} catch (IOException e) {
+								// TODO Auto-generated catch block
+								//Log.d(DTAG, "File " + ohCrap.getAbsolutePath() + "had an IO Exception");
+								//e.printStackTrace();
+							}
+						}
+
+					} // for
 				} while (!fileQueue.isEmpty());
 			} // onClick
 		}); // setOnClickListner
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
